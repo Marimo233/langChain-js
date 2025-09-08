cb2a64fce850018b2d114ed86423d3d49a949bb7
marimo <1072452597@qq.com>
2025-09-08 17:54:47 +0800

    引入消息模版

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const init = async () => {
   model: "qwen3:8b", 
 });
 
-const config = { configurable: { thread_id: uuidv4() } };
+// const config = { configurable: { thread_id: uuidv4() } };
 
 
 const promptTemplate = ChatPromptTemplate.fromMessages([
@@ -36,19 +36,19 @@ const promptTemplate = ChatPromptTemplate.fromMessages([
   ["placeholder", "{messages}"],
 ]);
 
-const input = [
-  {
-    role: "user",
-    content: "我是马里莫",
-  },
-];
+// const input = [
+//   {
+//     role: "user",
+//     content: "我是马里莫",
+//   },
+// ];
 
 
 
-const callModel = async (state) => {
-  const response = await ollamaModel.invoke(state.messages);
-  return { messages: response };
-};
+// const callModel = async (state) => {
+//   const response = await ollamaModel.invoke(state.messages);
+//   return { messages: response };
+// };
 
 const callModel2 = async (state) => {
   const prompt = await promptTemplate.invoke(state);
@@ -57,11 +57,11 @@ const callModel2 = async (state) => {
 };
 
 // Define a new graph
-const workflow = new StateGraph(MessagesAnnotation)
-  // Define the node and edge
-  .addNode("model", callModel)
-  .addEdge(START, "model")
-  .addEdge("model", END);
+// const workflow = new StateGraph(MessagesAnnotation)
+//   // Define the node and edge
+//   .addNode("model", callModel)
+//   .addEdge(START, "model")
+//   .addEdge("model", END);
 
 
   const workflow2 = new StateGraph(MessagesAnnotation)
@@ -70,39 +70,39 @@ const workflow = new StateGraph(MessagesAnnotation)
   .addEdge("model", END);
 
 
-  // Add memory
-const memory = new MemorySaver();
+//   // Add memory
+// const memory = new MemorySaver();
 
-const app = workflow.compile({ checkpointer: memory });
+// const app = workflow.compile({ checkpointer: memory });
 
 const app2 = workflow2.compile({ checkpointer: new MemorySaver() });
 
-const output = await app.invoke({ messages: input }, config);
-console.log(output.messages[output.messages.length - 1]);
+// const output = await app.invoke({ messages: input }, config);
+// console.log(output.messages[output.messages.length - 1]);
 
 
-const input2 = [
-  {
-    role: "user",
-    content: "我的名字是什么",
-  },
-];
-const output2 = await app.invoke({ messages: input2 }, config);
-console.log(output2.messages[output2.messages.length - 1]);
+// const input2 = [
+//   {
+//     role: "user",
+//     content: "我的名字是什么",
+//   },
+// ];
+// const output2 = await app.invoke({ messages: input2 }, config);
+// console.log(output2.messages[output2.messages.length - 1]);
 
 
-const config2 = { configurable: { thread_id: uuidv4() } };
+// const config2 = { configurable: { thread_id: uuidv4() } };
 
 const config3 = { configurable: { thread_id: uuidv4() } };
 
-const input3 = [
-  {
-    role: "user",
-    content: "我的名字是什么",
-  },
-];
-const output3 = await app.invoke({ messages: input3 }, config2);
-console.log(output3.messages[output3.messages.length - 1]);
+// const input3 = [
+//   {
+//     role: "user",
+//     content: "我的名字是什么",
+//   },
+// ];
+// const output3 = await app.invoke({ messages: input3 }, config2);
+// console.log(output3.messages[output3.messages.length - 1]);
 
 const input4 = [
   {
@@ -113,6 +113,14 @@ const input4 = [
 const output4 = await app2.invoke({ messages: input4 }, config3);
 console.log(output4.messages[output4.messages.length - 1]);
 
+const input5 = [
+  {
+    role: "user",
+    content: "我的名字是什么",
+  },
+];
+const output6 = await app2.invoke({ messages: input5 }, config3);
+console.log(output6.messages[output6.messages.length - 1]);
 
 };
 
